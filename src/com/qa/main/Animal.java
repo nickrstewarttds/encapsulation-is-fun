acb9f5cd5d94bc1108ee22c14e03fe836c4672b4
Nick Johnson <nickrstewarttds@gmail.com>
2020-03-26 15:12:39 +0000

    i can't spell

---
+++ src/com/qa/main/Animal.java
@@ -0,0 +1,26 @@
+package com.qa.main;
+
+//Entity
+public class Animal {
+
+	// Fields
+	private int eyes = 12;
+	private String name;
+
+	public int getEyes() {
+		return eyes;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setEyes(int eyes) {
+		this.eyes = eyes;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/com/qa/main/Runner.java
@@ -0,0 +1,19 @@
+package com.qa.main;
+
+public class Runner {
+
+	public static void main(String[] args) {
+
+		Animal cow = new Animal();
+
+		cow.setEyes(17);
+		System.out.println(cow.getEyes());
+
+		Animal chicken = new Animal();
+		System.out.println(chicken.getEyes());
+		chicken.setEyes(54236);
+		System.out.println(chicken.getEyes());
+
+	}
+
+}
